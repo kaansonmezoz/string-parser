677c1adca2dcb872a81369b2301f7dd5852a16c3
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2020-04-30 14:21:28 +0300

    Added out/ and build/

--- src/main/java/com/kaansonmezoz/objectutils/parser/annotation/PlaceHolder.java
+++ src/main/java/com/kaansonmezoz/objectutils/parser/annotation/PlaceHolder.java
@@ -1,4 +1,4 @@
-package com.kaansonmezoz.objectutils.annotations;
+package com.kaansonmezoz.objectutils.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
