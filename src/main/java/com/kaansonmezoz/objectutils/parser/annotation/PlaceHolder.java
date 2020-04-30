e424dbde88e89196ff882c79dad4b5c193d31429
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2020-04-30 14:53:30 +0300

    Moved package

--- src/main/java/com/kaansonmezoz/objectutils/parser/annotation/PlaceHolder.java
+++ src/main/java/com/kaansonmezoz/objectutils/parser/annotation/PlaceHolder.java
@@ -1,4 +1,4 @@
-package com.kaansonmezoz.objectutils.annotation;
+package com.kaansonmezoz.objectutils.parser.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
