80b7e02f649a815ce5001716673b9edc061ca814
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2020-05-01 18:29:56 +0300

    Implemented short primitive setter

--- src/main/java/com/kaansonmezoz/objectutils/setter/primitive/ShortSetter.java
+++ src/main/java/com/kaansonmezoz/objectutils/setter/primitive/ShortSetter.java
@@ -1,4 +1,14 @@
 package com.kaansonmezoz.objectutils.setter.primitive;
 
-class ShortSetter {
+import java.lang.reflect.Field;
+
+class ShortSetter extends PrimitiveFieldSetter{
+    ShortSetter() {
+        super(short.class);
+    }
+
+    @Override
+    public void set(Object object, Field field, String value) throws IllegalAccessException {
+        field.setShort(object, Short.parseShort(value));
+    }
 }
