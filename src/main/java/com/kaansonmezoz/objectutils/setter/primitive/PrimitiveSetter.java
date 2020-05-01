b48274f5ed252384cad10ef711fe40471c26daf7
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2020-05-01 18:29:13 +0300

    Created primitive field setters in PrimitiveSetter.

--- src/main/java/com/kaansonmezoz/objectutils/setter/primitive/PrimitiveSetter.java
+++ src/main/java/com/kaansonmezoz/objectutils/setter/primitive/PrimitiveSetter.java
@@ -3,17 +3,33 @@ package com.kaansonmezoz.objectutils.setter.primitive;
 import com.kaansonmezoz.objectutils.setter.FieldSetter;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class PrimitiveSetter implements FieldSetter {
     private final HashMap<Class, PrimitiveFieldSetter> setters;
 
     public PrimitiveSetter() {
-        this.setters = null;
+        this.setters = new HashMap<>();
+        createSetters().forEach((setter) -> setters.put(setter.getFieldType(), setter));
     }
 
     @Override
     public void set(Object object, Field field, String value) throws IllegalAccessException {
         setters.get(field.getType()).set(object, field, value);
+    }
+
+    private List<PrimitiveFieldSetter> createSetters() {
+        return Arrays.asList(
+                new BooleanSetter(),
+                new ByteSetter(),
+                new CharSetter(),
+                new DoubleSetter(),
+                new FloatSetter(),
+                new IntSetter(),
+                new LongSetter(),
+                new ShortSetter()
+        );
     }
 }
